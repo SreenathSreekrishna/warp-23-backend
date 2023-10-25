6c96af8e9a3fe40d4b5c9f080dfcd1f43a9c7497
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2023-10-25 11:38:35 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,jsonify,request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return jsonify({"ip":request.remote_addr})
-if __name__ == "__main__":
-    app.run()
