381c8c5a58728773efbf0238391c16cd4c5bc64a
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2023-10-26 10:35:04 +0530

    Update index.pyewdfew

--- api/index.py
+++ api/index.py
@@ -27,7 +27,7 @@ def index():
 
 def cors_allow(thing):
     response = make_response(thing)
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = "cosmoshield.neevsahay.com"
     return response
 
 @app.route('/verify', methods=['POST'])
